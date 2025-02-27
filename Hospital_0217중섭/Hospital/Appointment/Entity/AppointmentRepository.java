7cbade09186072090ce4c1e50cdbd87de8a87b15
cd991478 <cd991478@naver.com>
2025-02-27 15:46:16 +0900

    delete

--- Hospital_0217중섭/Hospital/Appointment/Entity/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package Hospital.Appointment.Entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-	
-}
--- Hospital_0217중섭/Hospital/User/Entity/UserInfoRepository.java
+++
@@ -1,6 +0,0 @@
-package Hospital.User.Entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo,String> {
-}
